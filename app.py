4f104eb8abaaa8ec0c89a38ed220bef1a6a4bdea
Arif Pambudi <arifpambudi242@gmail.com>
2024-07-19 14:24:46 +0700

    Refactor app.py to run on localhost instead of 0.0.0.0

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def search(username):
   return "Search result for {}".format(username)
 
 if __name__ == "__main__":
-  app.run(debug=True, port=8000, host="0.0.0.0")
+  app.run(debug=True, port=8000, host="127.0.0.1")
